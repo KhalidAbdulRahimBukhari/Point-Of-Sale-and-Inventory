c49fe10d4731efa9b02a86618946d665bba54854
khalid <k.bukhari.1418@gmail.com>
2026-01-16 17:22:46 +0100

    Addition: Added JWT skeleton

--- POSsystem.Api/Program.cs
+++ POSsystem.Api/Program.cs
@@ -1,36 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using POSsystem.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using POSsystem.Api.Models; // Don't forget this!
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddDbContext<PosDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("PosDb")));
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-
-
-app.UseHttpsRedirection();
-
+app.UseAuthentication(); // MUST be before Authorization
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
