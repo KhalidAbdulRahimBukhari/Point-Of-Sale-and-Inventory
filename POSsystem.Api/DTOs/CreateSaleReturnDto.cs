8ad23706150d88550db8a9df63977345c2a6d94c
khalid <k.bukhari.1418@gmail.com>
2026-01-17 22:15:33 +0100

    Modified : modified CreateSaleDto.cs

---
+++ POSsystem.Api/DTOs/CreateSaleReturnDto.cs
@@ -0,0 +1,16 @@
+﻿namespace POSsystem.Api.DTOs
+{
+    public class CreateSaleReturnDto
+    {
+        public string InvoiceNumber { get; set; }
+        public List<ReturnItemDto> Items { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+
+    public class ReturnItemDto
+    {
+        public int SaleItemId { get; set; }
+        public int QuantityToReturn { get; set; }
+    }
+
+}
