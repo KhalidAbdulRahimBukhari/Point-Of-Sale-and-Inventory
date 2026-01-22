a1030b5600ee608ba265f7ceaf38e189a1d99fb9
khalid <k.bukhari.1418@gmail.com>
2026-01-22 19:43:25 +0100

    EF: Added CreateUser DTO

--- POSsystem.Api/DTOs/CreateUsrDto.cs
+++ POSsystem.Api/DTOs/CreateUsrDto.cs
@@ -1,6 +1,17 @@
 ﻿namespace POSsystem.Api.DTOs
 {
-    public class CreateUsrDto
+    public class CreateUserDto
     {
+        // Person
+        public string FullName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+
+        // User
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int RoleId { get; set; }
+        public int ShopId { get; set; }
     }
+
 }
