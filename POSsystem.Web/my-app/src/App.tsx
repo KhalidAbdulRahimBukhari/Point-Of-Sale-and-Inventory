016444a16917f0c67f49fb37ab4d850d06ca1c20
khalid <k.bukhari.1418@gmail.com>
2026-02-06 18:19:06 +0100

    feat(frontend): added App.tsx + main.tsx

--- POSsystem.Web/my-app/src/App.tsx
+++ POSsystem.Web/my-app/src/App.tsx
@@ -1,9 +1,5 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import DashboardLayout from "./layout/DashboardLayout";
-
 // Pages
 import LoginPage from "./pages/auth/Login";
 import Dashboard from "./pages/Dashbaord/Dashboard";
@@ -12,18 +8,9 @@ import Cashier from "./pages/Cahier/Cashier";
 import Returns from "./pages/Returns/Returns";
 import StockInPage from "./pages/StockIn/StockInPage";
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: { main: "#84acd3" },
-    secondary: { main: "#9546a3" },
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
 
       <Routes>
         {/* Public */}
@@ -42,7 +29,7 @@ function App() {
         {/* Fallback */}
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
-    </ThemeProvider>
+
   );
 }
 
